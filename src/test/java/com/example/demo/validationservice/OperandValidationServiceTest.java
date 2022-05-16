effb2e8ccedbf51f770a3c88a52215650c29d21a
Divyansh Dheer <divyanshdheer42@gmail.com>
2022-05-16 14:31:25 +0530

    Update :+1:
    Added test case for Operand Validation Service class.

--- src/test/java/com/example/demo/validationservice/OperandValidationServiceTest.java
+++ src/test/java/com/example/demo/validationservice/OperandValidationServiceTest.java
@@ -1,11 +1,47 @@
 package com.example.demo.validationservice;
 
+import com.example.demo.exceptions.OperandDataException;
+import com.example.demo.exceptions.OperationException;
+import com.example.demo.operand.Operand;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 class OperandValidationServiceTest {
 
+    Operand operand;
+    OperandValidationService operandValidationService;
+
+    @BeforeEach
+    public void objectInit() {
+        operand = new Operand();
+        operandValidationService = new OperandValidationService();
+    }
+
     @Test
-    void objectValidation() {
+    @DisplayName("Test case for Custom Operation exception i.e valid inputs & invalid operation")
+    public void invalidOperationTestCase() {
+        operand.setFirstNumber("67.0");
+        operand.setSecondNumber("57.0");
+        operand.setOperation("add");
+
+        OperationException thrown = Assertions.assertThrows(OperationException.class, () -> operandValidationService.objectValidation(operand));
+        Assertions.assertTrue(thrown.toString().contains("Exception : Please add valid operation"));
+
+
+    }
+
+    @Test
+    @DisplayName("Test case for custom operand exception i.e invalid inputs")
+    public void invalidInputTestCase() {
+        operand.setFirstNumber("abc");
+        operand.setSecondNumber("57.0");
+        operand.setOperation("add");
+
+        OperandDataException thrown = Assertions.assertThrows(OperandDataException.class, () -> operandValidationService.objectValidation(operand));
+        Assertions.assertTrue(thrown.toString().contains("Exception : Please enter correct input !"));
+
     }
 }
